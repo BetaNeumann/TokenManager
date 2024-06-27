b548d8f95bc7b6395e1ac923f87b95785d8d6913
BetaNeumann <brunomoretti100@gmail.com>
2024-06-27 16:43:49 -0400

    Added some cli commands

--- tkmanager/cli.py
+++ tkmanager/cli.py
@@ -1,12 +1,16 @@
-from typing import Optional, Any
+from typing import Optional
 from typing_extensions import Annotated
 from datetime import datetime
 
 import typer
 from rich import print
-from cryptography.fernet import Fernet
+from rich.text import Text
+from rich.tree import Tree
 
 from tkmanager import _get_name, _get_version
+from .manager import Manager, Token
+from . import exceptions as ex, messages as msgs
+
 
 app = typer.Typer(
     name=_get_name(),
@@ -15,9 +19,12 @@ app = typer.Typer(
 )
 
 
+MANAGER = Manager()
+
+
 def _version(called: bool) -> None:
     if called:
-        print(f"[bright_cyan]{_get_name()} v{_get_version()}[/bright_cyan]")
+        print(f"[bright_cyan]{_get_name()} v{_get_version()}")
         raise typer.Exit()
 
 
@@ -33,3 +40,61 @@ def main(
     )
 ) -> None:
     """Manages the safe storage of Tokens!"""
+
+
+@app.callback()
+def make_file() -> None:
+    try:
+        MANAGER.make_file()
+    except ex.FileOverwriteError:
+        print('[red]File already exists...')
+        return
+
+
+@app.command()
+def store(
+    token_name: str,
+    token: str,
+    group: Annotated[
+        str, typer.Option(
+            '--group', '-g',
+            help='Group used to avoid name collisions.',
+            show_default=False
+        )
+    ] = 'DEFAULT',
+    expires: Annotated[
+        Optional[datetime], typer.Option(
+            '--expires', '-e',
+            help='Define an expiration date for the token.',
+            show_default=False
+        )
+    ] = None
+):
+    new_token = Token(token, expires)
+    MANAGER.store_token(new_token, token_name, group)
+
+    msg = f"The token '{token_name}' "
+    if group != 'DEFAULT':
+        msg += f"from the group '{group}' "
+    msg += f"was {msgs.SUCCESSFULLY} stored."
+    print(msg)
+
+
+@app.command(name='list')
+def list_() -> None:
+    data = MANAGER.read_data(False)
+    if data == {'DEFAULT': {}}:
+        print('No tokens stored...')
+        return
+
+    tree = Tree('Tokens', hide_root=True, style='bold')
+    for group, tokens in sorted(
+        data.items(),
+        key=lambda x: (0, '') if x == 'DEFAULT' else (1, x)
+    ):
+        group_tree = tree.add(group)
+        for token_name, token in sorted(tokens.items()):
+            token_description = f"{token_name}: {token['expires']}"
+            group_tree.add(Text(token_description, 'red'))
+
+    print(tree)
