c7bb139b24ff87a07b7721018f8e7aca1be10385
BetaNeumann <brunomoretti100@gmail.com>
2024-06-27 16:43:28 -0400

    Added file existence warning

--- tkmanager/__main__.py
+++ tkmanager/__main__.py
@@ -1,9 +1,12 @@
+from rich import print
 from . import cli
 
 
-def main() -> None:
-    cli.app()
-
-
 if __name__ == '__main__':
-    main()
+    if not cli.MANAGER.has_file():
+        print(
+            "[red]WARNING! NO FILE FOUND!",
+            "Create one with the command [bright_cyan]'tkmanager make_file'[/bright_cyan].",
+            sep='\n', end=''
+        )
+    cli.app()
